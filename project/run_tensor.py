fix(broadcast_index): fixed broadcast_index in tensor_data.py

project/run_tensor.py
@@ -4,27 +4,30 @@ Be sure you have minitorch installed in you Virtual Env.
 """
 
 import minitorch
+import torch
 
 # Use this function to make a random parameter in
 # your module.
 def RParam(*shape):
     r = 2 * (minitorch.rand(shape) - 0.5)
+    r = minitorch.zeros(shape) + 0.8
     return minitorch.Parameter(r)
 
-# TODO: Implement for Task 2.5.
 
 class Network(minitorch.Module):
     def __init__(self, hidden_layers):
         super().__init__()
         self.layer1 = Linear(2, hidden_layers)
-        self.layer2 = Linear(hidden_layers, hidden_layers)
-        self.layer3 = Linear(hidden_layers, 1)
+        # self.layer2 = Linear(hidden_layers, hidden_layers)
+        # self.layer3 = Linear(hidden_layers, 1)
 
     def forward(self, x):
-        middle = self.layer1.forward(x).relu()
-        end = self.layer2.forward(middle).relu()
-        return self.layer3.forward(end).sigmoid()
+        # middle = self.layer1.forward(x).relu()
+        # end = self.layer2.forward(middle).relu()
+        # output = self.layer3.forward(end).sigmoid()
+        # return output
 
+        return self.layer1.forward(x).relu()
 
 class Linear(minitorch.Module):
     def __init__(self, in_size, out_size):
@@ -35,19 +38,40 @@ class Linear(minitorch.Module):
 
 
     def forward(self, inputs):
-        """weight matrix mult input then add bias, output a tensor"""
-        # type Parameter .value is the tensor
-        # inputs = list of tensors, each representing a data point initially
-        # return self.weights.value * inputs + self.bias.value
-
-        # initially call fwd on dataset which has shape (num_pts, 2) = (num_pts, in_size) for first layer
         num_pts, in_size = inputs.shape
         # first layer weight has shape (in_size, out_size) = (2, hidden_layers)
         # want to broadcast so that (1, in_size, out_size) and (num_pts, in_size, 1)
-        matix_mul = (self.weights.value.view(1, in_size, self.out_size) * inputs.view(num_pts, in_size, 1)).sum(1).view(num_pts, self.out_size)
+        w = self.weights.value # (1, in_size, out_size)
+        xs = inputs.view(num_pts, in_size, 1)
+        matix_mul = (w*xs).sum(1).view(num_pts, self.out_size)
         # result is (num_pts, out_size), i.e. one output for each datapoint
+        return matix_mul + self.bias.value # bias shape is (1, out_size)
 
-        return matix_mul + self.bias.value.view(1, self.out_size)
+
+
+class PyTorchLinear(torch.nn.Module):
+    def __init__(self, in_size, out_size):
+        super().__init__()
+        self.weights = torch.nn.Parameter(torch.ones(in_size, out_size) - 0.2)
+        self.bias = torch.nn.Parameter(torch.ones(out_size)-0.2)
+
+    def forward(self, x):
+        return torch.matmul(x, self.weights) + self.bias
+
+class PyTorchNetwork(torch.nn.Module):
+    def __init__(self, hidden_layers):
+        super().__init__()
+        # self.layer1 = PyTorchLinear(2, hidden_layers)
+        # self.layer2 = PyTorchLinear(hidden_layers, hidden_layers)
+        # self.layer3 = PyTorchLinear(hidden_layers, 1)
+        self.layer1 = PyTorchLinear(2, hidden_layers)
+
+    def forward(self, x):
+        # middle = torch.relu(self.layer1(x))
+        # end = torch.relu(self.layer2(middle))
+        # output = torch.sigmoid(self.layer3(end))
+        # return output
+        return torch.relu(self.layer1(x))
 
 
 
@@ -101,8 +125,50 @@ class TensorTrain:
 
 
 if __name__ == "__main__":
-    PTS = 50
+    PTS = 10
     HIDDEN = 2
-    RATE = 0.5
+    RATE = 0.1
     data = minitorch.datasets["Simple"](PTS)
-    TensorTrain(HIDDEN).train(data, RATE)
+    # TensorTrain(HIDDEN).train(data, RATE)
+
+    # Create and run MiniTorch model
+    minitorch_model = Network(HIDDEN)
+    minitorch_input = minitorch.tensor(data.X)
+    minitorch_output = minitorch_model.forward(minitorch_input)
+    # print(minitorch_output)
+
+    minitorch_loss = minitorch_output.sum()
+    minitorch_loss.backward()
+
+    print(f"Minitorch Loss: {minitorch_loss}")
+
+    # print(minitorch_loss.grad)
+    # print(minitorch_model.grad)
+
+    print(minitorch_model.named_parameters)
+    print(minitorch_model.layer1)
+    print("LAYER 1 GRAD")
+    print(minitorch_model.layer1.weights.value.grad)
+    print(minitorch_model.layer1.bias.value.grad)
+
+
+    # Create and run PyTorch model
+    pytorch_model = PyTorchNetwork(HIDDEN)
+    pytorch_input = torch.tensor(data.X, dtype=torch.float32)
+    pytorch_output = pytorch_model(pytorch_input)
+    # print(pytorch_output)
+
+    pytorch_loss = pytorch_output.sum()
+    pytorch_loss.backward()
+
+    print(f"Pytorch Loss: {pytorch_loss}")
+
+    print(pytorch_model.named_parameters)
+    print(pytorch_model.layer1)
+    print("LAYER 1 GRAD")
+    print(pytorch_model.layer1.weights.grad)
+    print(pytorch_model.layer1.bias.grad)
+
+
+    # for name, param in pytorch_model.named_parameters():
+    #     print(f"{name} grad: {param.grad}")